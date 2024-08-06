9425f2f9ef49c4cc7ddc4d306f3112c188cc46d2
sum424 <sumanthapa326@gmail.com>
2024-08-06 17:49:17 -0400

    week 12-14 update

---
+++ js/faq.js
@@ -0,0 +1,29 @@
+
+    // DOM Elements
+    const tabs = document.querySelectorAll('.tab')
+    const tabContents = document.querySelectorAll('.tabcontent')
+      
+    // Functions
+    const activateTab = tabnum => {
+        
+        tabs.forEach( tab => {
+          tab.classList.remove('active')
+        })
+        
+        tabContents.forEach( tabContent => {
+            tabContent.classList.remove('active')
+        })
+    
+        document.querySelector('#tab' + tabnum).classList.add('active')
+        document.querySelector('#tabcontent' + tabnum).classList.add('active')
+        localStorage.setItem('jstabs-opentab', JSON.stringify(tabnum))
+    }
+    
+    // Event Listeners
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            activateTab(tab.dataset.tab)
+        })
+    })
+    
+    activateTab(opentab)
